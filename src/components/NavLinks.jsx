406190a2afa77f830deb4228b186311783026907
Danbethwel <93147941+Danbethwel@users.noreply.github.com>
2023-10-02 13:25:38 +0300

    new commit

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -21,7 +21,8 @@ const NavLinks = () => {
                 className="absolute inset-0 rounded-lg bg-gray-100"
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
-                animate={{ opacity: 1, transition: { duration: 0.15 } }}
+                animate={{ opacity: 1, 
+                            transition: { duration: 0.15 } }}
                 exit={{
                   opacity: 0,
                   transition: { duration: 0.15, delay: 0.2 },
